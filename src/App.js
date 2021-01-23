f10ad36ce0e4f7d59b55a0da90f437b298d10918
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-22 21:19:30 -0700

    saving work

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import DirectoryData from './DirectoryData';
 import Buttons from './Buttons';
-//import Header from './Header';
+
 
 
 function App() {
   return (
     <div className="App">
-   
+   <h1>Home</h1>
     <DirectoryData />
     <Buttons/>
 
--- src/DirectoryData.js
+++ src/DirectoryData.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import Buttons from 'react';
 import data from './data';
-//import Header from './Header';
+
 
 
 class DirectoryData extends Component {
@@ -24,15 +24,16 @@ render (){
     
     return(
         <div className="directoryData">
-         <h1>Home</h1>   
-
          <div>
          <h2 id="descrip">Name</h2>
-        <h3 id="descrip">From: </h3>
-        <h3 id="descrip">Job Title: </h3>
-        <h3 id="descrip">Employer: </h3>
+            <p id="descrip"> 
+                From: <br></br>
+                Job Title: <br></br>
+                Employer: 
+        
+            </p>
         <br></br>
-        <h3>Favorite Movies:</h3>
+        <p>Favorite Movies:</p>
         <ol>
             <li></li>
             <li></li>
