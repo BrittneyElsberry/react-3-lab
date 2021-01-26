464aa394c3b229e2d28dd18cf61d07073c05e69d
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-26 10:13:01 -0700

    assignment finished

--- src/App.js
+++ src/App.js
@@ -15,19 +15,51 @@ this.state = {
 data: data,
 index: 0
 }
-
 }
 
 
+increment = ()=> {
+
+if (this.state.index < this.state.data.length - 1){
+this.setState({index: this.state.index + 1})
+
+} else {
+  this.setState({
+    index: 0
+  })
+}
+
+}
+
+decrement = ()=> {
+  if (this.state.index > 0){
+    this.setState({index: this.state.index - 1})
+    
+    } else {
+      this.setState({
+        index: this.state.data.length -1
+      })
+    }
+  
+}
+
+deletePerson = () => {
+  const {data, index} = this.state //destructering
+  data.splice(index, 1) //this will remove 1 index of the array when the delete button is clicked
+  this.setState({
+    data
+  })
+}
+// ^^ why did we create this in app.js. the answer is, the functions need to be wherever the state is acting on
+// any action we take that will update the piece of state. function needs to go there.
+// 
+
 render(){
-
-
-
   return (
   <div className="App">
    <Header />
     <Profile data={this.state.data[this.state.index]} />
-    <Buttons data={this.state.data[this.state.index]}/>
+    <Buttons increment={this.increment} decrement={this.decrement} deletePerson={this.deletePerson}/>
     </div>
 )
 
--- src/Buttons.js
+++ src/Buttons.js
@@ -5,26 +5,27 @@ import Profile from './Profile';
 
 const Buttons = (props)=> {
 
-    const handleClickPrevious=()=>{
-            //console.log('The button is clicked.')
-        //props.data[1]
-           }
+//     const handleClickPrevious=()=>{
+//             //console.log('The button is clicked.')
+//         //props.data[1]
+//            }
     
 
-    const handleClickNext=()=>{
+//     const handleClickNext=()=>{
     
-//         //console.log('The button is clicked.')
-//         const forward = props.data.map((elem, index)=>{
-//            return index = index+ 1;
-//        })
- }
-
+// //         //console.log('The button is clicked.')
+// //         const forward = props.data.map((elem, index)=>{
+// //            return index = index+ 1;
+// //        })
+//  }
+//THIS WAS INCORRECT. NEEDED TO BE DONE IN APP.js ^^^^^^^^^^^^^^^^^
 
 return(
 
     <div>
-    <button className="previous" onClick={handleClickPrevious}>{`< Previous`} {props.userDirectory}</button>
-    <button className="next" onClick={handleClickNext}>{`Next >`}</button>
+    <button className="previous" onClick={()=>props.decrement()}>{`< Previous`} {props.userDirectory}</button>
+    <button onClick={()=>props.deletePerson()}>Delete Person</button>
+    <button className="next" onClick={()=>props.increment()}>{`Next >`}</button>
 </div>
 
 )
